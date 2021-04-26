46d7df80eee82ce6c72b9d26e85b067464256213
NguyenTuongLe <tuongle200220@gmail.com>
2021-04-26 08:06:30 +0700

    form get & post

---
+++ PHP/FORM 2.php
@@ -0,0 +1,16 @@
+<p>Guessing game...</p>
+<form>
+    <p><label for="guess">Input Guess</label>
+        <input type="text" name="guess" id="guess"/></p>
+    <input type="submit"/>
+</form>
+<pre>
+$_GET:
+<?php
+print_r($_GET);
+?>
+</pre>
+
+
+<!--GET lay tren thanh url xuong va co the len thanh url-->
+<!--get la nhan-->
